707ba4114a2014de7bec48b5233390351b6461f2
devbotas <simon@devbot.lt>
2024-01-18 15:54:42 +0200

    Dropping CSharp stuff. It needs a different approach.

--- Tech.Tevux.Dashboards.Controls.Base/Code/ScriptContextBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tech.Tevux.Dashboards.Controls {
-    public abstract class ScriptContextBase {
-        public bool IsCancellationRequested { get; set; }
-        public abstract ISharedLibraryMessagingProvider Messenger { get; }
-        public void WriteLine(string text) {
-            Messenger.Send("editor-debug-output", new SetValueMessage(text));
-        }
-    }
-}
